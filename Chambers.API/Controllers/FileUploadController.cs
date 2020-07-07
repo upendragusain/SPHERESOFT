0140a31c0bd8a46c3739bed7d95edec8d51c1deb
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-07 16:20:47 +0100

    initial commit
    we have take off!

---
+++ Chambers.API/Controllers/FileUploadController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Chambers.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly ILogger<FileUploadController> _logger;
+
+        public FileUploadController(ILogger<FileUploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "We have take off!";
+        }
+    }
+}
---
+++ Chambers.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Chambers.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
